ee787345382cf0e5c7e29ae64358158245e04154
yuvrajsingh1911 <83699519+yuvrajsingh1911@users.noreply.github.com>
2021-08-04 08:48:50 +0530

    Add files via upload

---
+++ game.css
@@ -0,0 +1,63 @@
+body
+{
+	background-image: url("https://cdn.hipwallpaper.com/i/75/53/0cFvKO.jpg");
+}
+
+	.login_div1 , .login_div2
+	{
+		background: white;
+		padding: 10px;
+		float: none;
+		border-radius: 15px;
+		width: 30%;
+		height: 150px;
+		background-color: wheat;
+	}
+
+
+#player1_name , #player2_name
+{
+	display: inline-block;
+	margin-left: 3px;
+	color: tomato;
+	font-weight: bold;
+}
+#word
+{
+	width: 60%;
+}
+#word_display
+{
+	letter-spacing: 5px;
+}
+#output
+{
+background: white;
+border-radius: 10px;
+border-top: 10px solid #AA4465;
+padding: 10px;
+float: none;
+text-align: left;
+}
+h1{
+	color: orange;
+	font-family: 'Akaya Kanadaka', cursive;
+}
+h3{
+	font-family: 'Lora', serif;
+	color: teal;
+}
+label{
+	font-family: 'Yellowtail', cursive;
+	color: turquoise;
+}
+button{
+	font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+}
+input:hover{
+	background-color: yellow;
+}
+div:hover{
+	border: 5px solid rgb(102, 6, 73);
+	width: 35%;
+}
